514bab1b9a59c4eb0413f427879651245a20b62f
H4N4K8 <hannah.francis03@gmail.com>
2024-03-18 08:38:01 -0400

    Add project files.

---
+++ LambdaExpressions/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace LambdaExpressions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter two numbers to be evaluated.");
+            int x = int.Parse(Console.ReadLine());
+            int y = int.Parse(Console.ReadLine());  
+
+            //add
+            var sum = (int x, int y) => x + y;
+            //multipy
+            var product = (int x, int y) => x * y;
+
+            //smaller
+            var smallerValue = (int x, int y) =>
+            {
+                if (x < y)
+                    return x;
+                else
+                    return y;
+            };
+
+                Console.WriteLine($"The sum is: {sum(x,y)}");
+                Console.WriteLine($"The product is: {product(x,y)}");
+                Console.WriteLine($"The smallerr value is: {smallerValue(x,y)}");
+                Console.WriteLine();
+
+        }
+    }
+}
